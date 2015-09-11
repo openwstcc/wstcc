7e656baca4b81d668a4586e8395bb369dec04a09
TCC_SERVER_DEV <TCC_SERVER_DEV@SERVERDEV>
2015-09-10 21:24:53 -0300

    RespostaDAO

--- src/main/java/dao/DuvidaDAO.java
+++ src/main/java/dao/DuvidaDAO.java
@@ -2,10 +2,16 @@ package dao;
 
 import model.Duvida;
 import model.Materia;
+import model.Tag;
 import model.Usuario;
 import java.util.List;
 
 interface DuvidaDAO {
-	public void adicionarDuvida (Duvida d,Usuario u,Materia D);
-	public List <Duvida> listaDuvida();
+	public void adicionarDuvida (Duvida d,Usuario u,List <Materia> m, List<Tag> t);
+	public void alterarDuvida (Duvida d);
+	public void removerDuvida (Duvida d);
+	public List <Duvida> buscarDuvidasMateria (Materia m);
+	public List <Duvida> buscarDuvidasUsuario (Usuario u);
+	public List <Duvida> buscarDuvidasTags( Tag t);
+	
 }
--- src/main/java/dao/DuvidaDAOImplementation.java
+++ src/main/java/dao/DuvidaDAOImplementation.java
@@ -4,20 +4,46 @@ import java.util.List;
 
 import model.Duvida;
 import model.Materia;
+import model.Tag;
 import model.Usuario;
 
 public class DuvidaDAOImplementation implements DuvidaDAO {
 
 	@Override
-	public void adicionarDuvida(Duvida d, Usuario u, Materia D) {
+	public void adicionarDuvida(Duvida d, Usuario u, List<Materia> m, List<Tag> t) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public List<Duvida> listaDuvida() {
+	public void alterarDuvida(Duvida d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removerDuvida(Duvida d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Duvida> buscarDuvidasMateria(Materia m) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public List<Duvida> buscarDuvidasUsuario(Usuario u) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Duvida> buscarDuvidasTags(Tag t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
