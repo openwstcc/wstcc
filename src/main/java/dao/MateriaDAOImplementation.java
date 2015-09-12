05775cd1a9a1bb5c69a90af7d603cf13281df8a7
TCC_SERVER_DEV <TCC_SERVER_DEV@SERVERDEV>
2015-09-12 18:59:18 -0300

    Implementação buscarMateriasUsuario

--- src/main/java/dao/MateriaDAOImplementation.java
+++ src/main/java/dao/MateriaDAOImplementation.java
@@ -39,8 +39,31 @@ public class MateriaDAOImplementation implements MateriaDAO {
 
 	@Override
 	public List<Materia> buscarMateriasUsuario(Usuario u) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Connection con = JDBCUtil.getInstance().getConnection();
+			PreparedStatement pstmt = con
+					.prepareStatement("SELECT M.ID_MATERIA, M.MATERIA, M.SEMESTRE FROM MATERIA_USUARIO AS MA "
+							+ "INNER JOIN USUARIO U ON MA.ID_USUARIO=U.ID_USUARIO INNER JOIN MATERIA M ON MA.ID_MATERIA=M.ID_MATERIA"
+							+ "WHERE U.ID_USUARIO=?");
+			pstmt.setInt(1, u.getIdUsuario());
+			ResultSet rs = pstmt.executeQuery();
+			List<Materia> materias = new ArrayList<Materia>();
+			while (rs.next()) {
+				Materia m = new Materia();
+				m.setIdMateria(rs.getInt("ID_MATERIA"));
+				m.setMateria(rs.getString("MATERIA"));
+				m.setSemestre(rs.getInt("SEMESTRE"));
+				materias.add(m);
+			}
+			pstmt.close();
+			return materias;
+
+		} catch (SQLException e) {
+			System.out.println("Erro ao carregar lista de Matérias");
+			System.out.println(e);
+			return null;
+		}
+
 	}
 
 	@Override
