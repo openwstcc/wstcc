8b33c978e8dd5a0da7b70815ed7954b47bbbef20
Gabriel Queiroz <gabriel.fatec@hotmail.com>
2015-09-16 14:22:32 -0300

    Alteração na classe de teste para utilização de interface DAO.

--- src/main/java/tests/UsuarioDAOTest.java
+++ src/main/java/tests/UsuarioDAOTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import java.util.Random;
 
+import dao.UsuarioDAO;
 import dao.UsuarioDAOImplementation;
 import model.Usuario;
 
@@ -19,28 +20,28 @@ public class UsuarioDAOTest {
 	}
 	
 	public static boolean insertUsuario(){
-		UsuarioDAOImplementation dao = new UsuarioDAOImplementation();
+		UsuarioDAO dao = new UsuarioDAOImplementation();
 		UsuarioTest u = new UsuarioTest();
 		return dao.adicionarUsuario(u.randomNoID());
 	}	
 	
 	public static boolean alterarUsuario(){
-		UsuarioDAOImplementation dao = new UsuarioDAOImplementation();
+		UsuarioDAO dao = new UsuarioDAOImplementation();
 		UsuarioTest u = new UsuarioTest();
 		return dao.alterarUsuario(u.randomID());
 	}
 	
 	public static boolean alterarSenha(){
-		UsuarioDAOImplementation dao = new UsuarioDAOImplementation();
+		UsuarioDAO dao = new UsuarioDAOImplementation();
 		UsuarioTest u = new UsuarioTest();
 		return dao.alterarSenha(u.randomID());
 	}
 	
 	public static Usuario selectUsuario(){
-		UsuarioDAOImplementation dao = new UsuarioDAOImplementation();	
+		UsuarioDAO dao = new UsuarioDAOImplementation();	
 		Usuario u = new Usuario();
 		Random r = new Random();
-		u.setIdUsuario(1);
+		u.setIdUsuario(r.nextInt(4)+1);
 		return dao.buscarUsuario(u);
 	}
 }
