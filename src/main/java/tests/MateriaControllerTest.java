3123e186d0ba9bf42386b9fca5981c735bfad68c
Gabriel Queiroz <gabriel.fatec@hotmail.com>
2015-09-18 11:29:53 -0300

    Teste de controle de Materia OK

--- src/main/java/tests/MateriaControllerTest.java
+++ src/main/java/tests/MateriaControllerTest.java
@@ -1,31 +1,44 @@
 package tests;
 
+import java.util.Random;
+
 import com.google.gson.Gson;
 
 import control.MateriaControl;
+import model.Duvida;
 
 public class MateriaControllerTest {
+	static MateriaControl control = new MateriaControl();;
+	static Gson gson = new Gson();;
+
 	public static void main(String[] args) {
 		// Seleciona todas as materias retornando JSon.
+		System.out.println("Todas as materias:");
 		System.out.println(selectMaterias());
 		// Seleciona todas as materias por usuario.
-		System.out.println(selectMateriasUsuario());
+		System.out.println("Todas as materias por usuário:");
+		System.out.println("Materias: " + selectMateriasUsuario());
+		// Seleciona todas as materias por duvida.
+		System.out.println("Todas as materias por dúvida:");
+		System.out.println("Materias: " + selectMateriasDuvida());
 	}
 
 	public static String selectMaterias() {
-		MateriaControl control = new MateriaControl();
 		return control.buscarMaterias();
 	}
 
 	public static String selectMateriasUsuario() {
-		MateriaControl control = new MateriaControl();
-		Gson gson = new Gson();
 		String usuario = gson.toJson(UsuarioDAOTest.selectUsuario());
 		System.out.println("Usuario JSON: " + usuario);
 		return control.buscarMateriasUsuario(usuario);
 	}
 
-	public static String selectMateriasDuvida() {		
-		return null;
+	public static String selectMateriasDuvida() {
+		Duvida d = new Duvida();
+		Random r = new Random();
+		d.setIdDuvida(r.nextInt(5)+1);
+		String duvida = gson.toJson(d);
+		System.out.println("Duvida JSON: " + duvida);
+		return control.buscarMateriasDuvida(duvida);
 	}
 }
