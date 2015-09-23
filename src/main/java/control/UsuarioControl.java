8ff0e50d26d66f9f689151e3ada07cc1f64a55b8
TCC_SERVER_DEV <TCC_SERVER_DEV@SERVERDEV>
2015-09-22 23:57:28 -0300

    Implementação UsuarioControl

--- src/main/java/control/UsuarioControl.java
+++ src/main/java/control/UsuarioControl.java
@@ -1,5 +1,12 @@
 package control;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import dao.UsuarioDAO;
+import dao.UsuarioDAOImplementation;
+import model.Usuario;
+
 /**
  * Usuario Controller responsável pelos métodos de usuários. Estes métodos são
  * disponibilizados a partir do Webservice e permitem a integração entre o
@@ -9,20 +16,34 @@ package control;
  * 
  */
 public class UsuarioControl {
+
+	private Gson objects = new Gson();
+	UsuarioDAO dao = new UsuarioDAOImplementation();
+
 	public boolean inserirUsuario(String jsonUsuario) {
-		return false;
+		Usuario u = objects.fromJson(jsonUsuario, Usuario.class);
+		boolean retorno = dao.adicionarUsuario(u);
+		return retorno;
 	}
 
 	public boolean alterarUsuario(String jsonUsuario) {
-		return false;
-	}
-	
-	public boolean alterarSenha(String jsonUsuario) {
-		return false;
+		Usuario u = objects.fromJson(jsonUsuario, Usuario.class);
+		boolean retorno = dao.alterarUsuario(u);
+		return retorno;
 	}
 
-	public String buscarUsuarios() {		
-		return null;
+	public boolean alterarSenha(String jsonUsuario) {
+		Usuario u = objects.fromJson(jsonUsuario, Usuario.class);
+		boolean retorno = dao.alterarSenha(u);
+		return retorno;
+	}
+
+	public String buscarUsuario(String jsonUsuario) {
+		Usuario u = objects.fromJson(jsonUsuario, Usuario.class);
+		Usuario usuario = dao.buscarUsuario(u);
+		Gson gson = new GsonBuilder().create();
+		String json = gson.toJson(usuario);
+		return json;
 	}
 
 }
